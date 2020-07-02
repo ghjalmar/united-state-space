d552a92fc072130c67828479a9583ce422914a01
ghjalmar <gudmundurhjalmare@gmail.com>
2020-07-02 23:36:18 +0200

    Some reference tracking is enabled!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as p
 import numpy as np
+import trackerClass
 
 x = np.linspace(0, 2*np.pi)
 y = [10*np.sin(val) for val in x]
@@ -10,4 +11,14 @@ ax.plot(x, y)
 ax1 = ax.twinx()
 ax1.plot(x, y2)
 
-p.show()
+#p.show()
+
+obj = trackerClass.tracker()
+obj.calc_gain()
+
+r0 = np.array([[1], [0]])
+r_i = r0
+for i in range(len(x)):
+    print(i)
+    obj.update_pos(r_i)
+    print(obj.state_space)
--- trackerClass.py
+++ trackerClass.py
@@ -1,18 +1,23 @@
 import numpy as np
 import control
 
-class tracker(x = 0, y = 0, theta = 0, v = 1):
+class tracker:
     # Object that tracks the reference.
-    def __init__(self, x, y, theta, v):
+    def __init__(self, x=0, y=0, theta=0, v=1, dt=1):
+        self.dt = dt
         # State space consists of [x, y, theta, v]^T
         self.state_space = np.transpose(np.array([x, y, theta, v]))
     def update_pos(self, reference):
-        # Updates the position of the tracker object. Takes in an np.array with x and y pos of object to track.
+        # Updates the position of the tracker object. Takes in an np.array with control input dV and dTheta.
+        self.state_space[0] = self.state_space[0] + np.cos(reference[1])*reference[0]*self.dt
+        self.state_space[1] = self.state_space[1] + np.sin(reference[1])*reference[0]*self.dt
+        self.state_space[2] = self.state_space[2] + reference[0]
+        self.state_space[3] = self.state_space[3] + reference[1]
 
     def calc_gain(self):
         # Calculate new gain.
-        self.A = np.identity(4)
-        self.B = np.array([[cos(self.theta), 0], [sin(self.theta), 0], [0, 1], [1, 0]]) # Needs to be linearized!
-        self.Q = np.identity(4)
-        self.R  = np.identity(4)
+        self.A = np.identity(2)
+        self.B = np.identity(2)
+        self.Q = np.identity(2)
+        self.R  = np.identity(2)
         self.K, _, _ = control.lqr(self.A, self.B, self.Q, self.R)
