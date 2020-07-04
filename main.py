9055306523c9eb16dbe3516a13515a7d90d22759
ghjalmar <gudmundurhjalmare@gmail.com>
2020-07-04 12:28:48 +0200

    feedback loop works

--- main.py
+++ main.py
@@ -2,23 +2,54 @@ import matplotlib.pyplot as p
 import numpy as np
 import trackerClass
 
-x = np.linspace(0, 2*np.pi)
-y = [10*np.sin(val) for val in x]
-y2 = [10*np.cos(val) for val in x]
-fig, ax = p.subplots()
-ax.plot(x, y)
+# Consant
+x_t = np.linspace(1, 10, 200)
+y_t = [1 for val in x_t]
 
-ax1 = ax.twinx()
-ax1.plot(x, y2)
+# Sinus
+#x_t = np.linspace(0, np.pi/4, 100)
+#y_t = [10*np.sin(0.01*val) for val in x_t]
+#y2_t = [10*np.cos(val) for val in x_t]
+
+# Linear
+#x_t = np.linspace(1, 10, 10)
+#y_t = [2*val for val in x_t]
+
+p.plot(x_t, y_t)
+#p.plot(x_t, y2_t)
 
 #p.show()
 
+# Setting up the object
 obj = trackerClass.tracker()
 obj.calc_gain()
 
-r0 = np.array([[1], [0]])
+# Initial reference
+r0 = np.array([[0.1], [0]])
 r_i = r0
-for i in range(len(x)):
-    print(i)
-    obj.update_pos(r_i)
+
+# Arrays to store the location of the object that is tracking the reference
+x_pos = np.zeros((2, len(x_t)))
+dTheta = np.zeros((1, len(x_t)))
+for i in range(1, len(x_t)):
+    print(r_i)
+    print('State space:')
     print(obj.state_space)
+    obj.update_pos(r_i)
+    x_pos[0, i] = obj.state_space[0]
+    x_pos[1, i] = obj.state_space[1]
+    temp_Kx = np.matmul(obj.K, r_i)
+    d = np.sqrt((x_pos[0, i] - x_pos[0, i-1])**2 + (x_pos[1, i] - x_pos[1, i-1])**2)
+    print(f'd: {d}')
+    temp_theta = np.arcsin(obj.dx/d)
+    print(f'temp theta: {temp_theta}')
+    dTheta[0,i] = temp_theta
+    r_i[1] = temp_theta
+    r_i[0] = r_i[0] + 0.1
+    r_i = r_i - temp_Kx
+
+p.plot(x_pos[0,:], x_pos[1,:], color='tab:red')
+p.show()
+p.figure()
+p.plot(dTheta[0,:], color='tab:red')
+p.show()
--- trackerClass.py
+++ trackerClass.py
@@ -6,18 +6,21 @@ class tracker:
     def __init__(self, x=0, y=0, theta=0, v=1, dt=1):
         self.dt = dt
         # State space consists of [x, y, theta, v]^T
-        self.state_space = np.transpose(np.array([x, y, theta, v]))
+        self.state_space = np.array([x, y, theta, v])
+        self.state_space = self.state_space.astype(np.float64)
+
     def update_pos(self, reference):
         # Updates the position of the tracker object. Takes in an np.array with control input dV and dTheta.
         self.state_space[0] = self.state_space[0] + np.cos(reference[1])*reference[0]*self.dt
         self.state_space[1] = self.state_space[1] + np.sin(reference[1])*reference[0]*self.dt
         self.state_space[2] = self.state_space[2] + reference[0]
         self.state_space[3] = self.state_space[3] + reference[1]
+        self.dx = np.cos(reference[1]) * reference[0] * self.dt
 
     def calc_gain(self):
         # Calculate new gain.
         self.A = np.identity(2)
         self.B = np.identity(2)
-        self.Q = np.identity(2)
-        self.R  = np.identity(2)
+        self.Q = 0.01*np.identity(2)
+        self.R  = 10*np.identity(2)
         self.K, _, _ = control.lqr(self.A, self.B, self.Q, self.R)
